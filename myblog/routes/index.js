5db8b99b4c86729c804599866eb5f8f59464addc
ch <807672412@qq.com>
2017-02-14 21:55:11 +0800

    更新微博首页

--- myblog/routes/index.js
+++ myblog/routes/index.js
@@ -12,4 +12,9 @@ router.post('/', function(req, res, next) {
 	skillmodel.myskillShow(req, res);
 });
 
+/*跳转微博首页*/
+router.get("/blogindex", function(req, res, next) {
+	res.render("blogindex");
+});
+
 module.exports = router;
