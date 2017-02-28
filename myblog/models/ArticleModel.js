d61309238c061b4084eda9f021dbc5cca2af40e6
ch <807672412@qq.com>
2017-02-28 14:07:08 +0800

    实现添加文章功能

--- myblog/models/ArticleModel.js
+++ myblog/models/ArticleModel.js
@@ -11,22 +11,50 @@ module.exports = {
 				res.send("获取连接错误,错误原因:" + err.message);
 				return;
 			}
+			var params = [req.body["title"], req.body["author"], req.body["pre"], req.body["content"], req.body["times"]];
+			async.series({
+				one: function(callback) {
+					if(req.body["btnspe"]) {
+						var speSql = "insert into spearticle (title, author, pre, content, times) value (?,?,?,?,?)";
 
-			if(req.body["btnspe"]) {
-				var speSql = "insert into spearticle (title, author, pre, content, times) value (?,?,?,?,?)";
-				var speparams = [req.body["title"], req.body["author"], req.body["pre"], req.body["content"], req.body["times"]];
+						conn.query(speSql, params, function(err, rs) {
+							if(err) {
+								res.send("获取连接错误,错误原因:" + err.message);
+								return;
+							}
 
-				conn.query(speSql, speparams, function(err, rs) {
-					if(err) {
-						res.send("获取连接错误,错误原因:" + err.message);
-						return;
+							callback(null, rs);
+						});
+					} else {
+						callback(null);
+					}
+				},
+				two: function(callback) {
+					console.log(req.body["btnsel"])
+					if(req.body["btnsel"] == "JavaScript") {
+						var insertSql = "insert into jsarticle (title, author, pre, content, times) value (?,?,?,?,?)";
+					} else if(req.body["btnsel"] == "NodeJS") {
+						var insertSql = "insert into nodearticle (title, author, pre, content, times) value (?,?,?,?,?)";
+					} else if(req.body["btnsel"] == "Web框架") {
+						var insertSql = "insert into webarticle (title, author, pre, content, times) value (?,?,?,?,?)";
 					}
 
-					res.json({
-						messages: "创建文章成功"
+					console.log(params);
+					conn.query(insertSql, params, function(err, rs) {
+						if(err) {
+							res.send("获取连接错误,错误原因:" + err.message);
+							return;
+						}
+
+						callback(null, rs);
 					});
+				}
+			}, function(err, results) {
+				console.log(results);
+				res.json({
+					messages: "创建文章成功"
 				});
-			}
+			});
 			conn.release();
 		});
 	},
@@ -40,30 +68,54 @@ module.exports = {
 			}
 
 			if(req.body["indexbig"]) {
-				var selectspeSql = "select title, author, pre, times from spearticle";
-				var params = [];
+				var selectspeSql = "select title, author, pre, times from spearticle limit ?,?";
+				var specountSql = "select count(*) from spearticle";
+
+				var pageSize = req.body["pagesize"];
+				var page = req.body["pagenum"];
+
+				//开始的页面数
+				var pointStart = (page - 1) * pageSize;
+				var params = [pointStart, pageSize];
 
 				//获取对象
 				var titleArr = [];
 
-				conn.query(selectspeSql, params, function(err, rs) {
-					if(err) {
-						res.send("获取连接错误,错误原因:" + err.message);
-						return;
-					}
+				async.series({
+					one: function(callback) {
+						conn.query(specountSql, [], function(err, rs) {
+							if(err) {
+								res.send("获取连接错误,错误原因:" + err.message);
+								return;
+							}
 
-					for(var i = 0; i < rs.length; i++) {
-						titleArr.push({
-							"title": rs[i].title,
-							"author": rs[i].author,
-							"pre": rs[i].pre,
-							"times": rs[i].times,
-							"len": (Math.floor((rs.length - 1) / req.body["num"]) + 1)
+							callback(null, rs);
+						});
+					},
+					two: function(callback) {
+						conn.query(selectspeSql, params, function(err, rs) {
+							if(err) {
+								res.send("获取连接错误,错误原因:" + err.message);
+								return;
+							}
+
+							for(var i = 0; i < rs.length; i++) {
+								titleArr.push({
+									"title": rs[i].title,
+									"author": rs[i].author,
+									"pre": rs[i].pre,
+									"times": rs[i].times
+								});
+							}
+							callback(null, rs);
 						});
 					}
+				}, function(err, results) {
 					res.json({
-						titleArr: titleArr
+						titleArr: titleArr,
+						result: Math.ceil(results.one[0]["count(*)"] / pageSize)
 					});
+					console.log(Math.ceil(results.one[0]["count(*)"] / pageSize))
 				});
 				conn.release();
 			}
